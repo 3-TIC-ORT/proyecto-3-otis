373d2b792bcb290d371dcf435719c3840fdbb206
Sofffisab <sofibels@gmail.com>
2024-08-06 15:47:42 -0300

    crear usuario pt1

--- Login/local storage.js
+++ Login/local storage.js
@@ -1,28 +1,49 @@
+let perfil = {
 
-//guardar_localstorage();  //  guardar datos
-//obtener_localstorage();  //  llamar datos
+    usuario: "sofi",
+    contraseña: "abcd",
+    subido: ["video 1"], // empieza con: nada
+    gusta: ["audio 1", "audio 2", "foto 2"], // empieza con: todos los elementos
+    noGusta: ["foto 1"], //  empieza con: nada
+
+} //  fin variable perfil
+
+
+guardar_localstorage();  //  guardar datos
+obtener_localstorage();  //  llamar datos
 
 function obtener_localstorage(){
 
-    if( localStorage.getItem( "perfil" )){
-        let perfil = JSON.parse( localStorage.getItem( "perfil" )); // llamo este dato 
+    if( localStorage.getItem( "perfil" )) {
+        let perfil = JSON.parse( localStorage.getItem( "perfil" )); // llamo este dato, y lo transformo en un objeto
     }
 }
-function guardar_localstorage(){
-
-    let perfil = {
-
-        usuario: "sofi1234",
-        contraseña: "abcde",
-        subido: "video 1",
-        gusta: ["audio 1", "audio 2", "foto 2"],
-        noGusta: ["foto 1"],
-
-    } //  fin variable perfil
-
+function guardar_localstorage() {
 
 localStorage.setItem( "perfil", JSON.stringify( perfil ) );
 
+obtener_localstorage();
+
+
+document.getElementById('pregunta1').addEventListener('submit', function(event) {
+    event.preventDefault(); // no se manda formulario
+
+    var nombreingresado = document.getElementById('usuario').value; // Obtiene el valor del campo de texto
+    
+   perfil.usuario = nombreingresado; // Asigna el valor a caracteristica de la variable
+   guardar_localstorage();
+
+});
+
+document.getElementById('pregunta2').addEventListener('submit', function(event) {
+    event.preventDefault(); // no se manda formulario
+
+    var nombreingresado = document.getElementById('contraseña').value; // Obtiene el valor del campo de texto
+    
+   perfil.contraseña = nombreingresado; // Asigna el valor a caracteristica de la variable
+   guardar_localstorage();
+
+});
 
 
 } //  fin función
