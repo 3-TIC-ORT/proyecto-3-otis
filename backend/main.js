cd49c6b21c7dbb576fd4cbc6b51971a332a0e085
Sofffisab <sofibels@gmail.com>
2024-08-09 14:45:53 -0300

    encriptar para despues

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,5 @@
 import  Express  from "express";
-
+import  argon2 from "argon2";
 
 // learn exxpress.JS in 15 minutes!
 
@@ -10,3 +10,7 @@ app.post("/enviar_datos", async (req, res) => { // endpoint
     console.log(datosUser.usuario)
     
 })
+
+
+
+//argon2.hash(contraseña, 2) // poner despues de que ingresa contraseña adentro de la función para encriptar
