f9fdfc22f86af39d9f26441190c99431aaddb199
Iair1 <iairgreb@gmail.com>
2024-10-01 08:21:42 -0300

    pantalla galeria programada

--- frontend/galeria/galeria otis.js
+++
@@ -1,7 +0,0 @@
-let nosabe = ["imagen 0.jpg", "imagen 1.jpg", "imagen 2.jpg"];
-for(let numero = 0; numero < nosabe.length; numero++){
-    document.getElementById(`img${numero}`).src = nosabe[numero]; 
-}
-for(let numere = 0; numere < 6; numere++){
-    document.getElementsByClassName("megustaono")[5-numere].style.width = `${numere*100}px`; //despues lo saco, es solo para acordrme de como hacer algo
-}
