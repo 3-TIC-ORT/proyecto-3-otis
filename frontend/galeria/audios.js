8051252c64b0c7ce4d3aa7df2c348b454b4381a1
Iair1 <iairgreb@gmail.com>
2024-10-29 08:39:05 -0300

    audios

---
+++ frontend/galeria/audios.js
@@ -0,0 +1,24 @@
+fetchData("audiosSubidos", ponerlosaudios);
+function ponerlosaudios(){
+    for(let i = 1; i < DataTransfer.length; i++){
+       let item = document.createElement("audio");
+       item.id = `audio${i}`;
+       item.src = data[i];
+       item.className = "audioOculto";
+       document.getElementById("ELBODY").appendChild(item);
+       let item2 = document.createElement("button");
+       item2.id = `boton${i}`;
+       item2.className = `audiofalso`;
+       item2.value = i
+       document.getElementById("audiosdeCoso").appendChild(item2);
+       document.getElementById(`boton${i}`).addEventListener("click", reproducir);
+    }
+}
+function reproducir(){
+    let oooo = document.getElementById(`audio${this.value}`);
+    if(oooo.paused){
+        oooo.play();
+    } else{
+        oooo.pause();
+    }
+}
