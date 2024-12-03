0866bdea547a5fe4e58fdf4c6f7392c83422123d
Iair1 <iairgreb@gmail.com>
2024-12-03 07:49:38 -0300

    .

--- frontend/Login/loginJS.js
+++ frontend/Login/loginJS.js
@@ -11,3 +11,17 @@ function seInicioSesion (){
     sesion.contraseña = document.getElementById("contraseña").value;
     console.log(sesion);
 }
+//fetchData("inicioSecion", incorrecto);//
+function incorrecto (){
+    if(data === 0){
+    document.getElementById("no").style.opacity = "1";
+    document.getElementById("nomensaje").innerHTML = "Error al iniciar usuario o contraseña";
+    setTimeout (() => {
+        document.getElementById("no").style.opacity = "0";
+        document.getElementById("nomensaje").innerHTML = "";
+    }, 3000);
+    } else {
+        window.location.href = "../Pantalla6.html"
+    }
+}
+iniciarSesion(sesion);
